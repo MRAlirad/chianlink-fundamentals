8d7c1039ed410dc98570a772ce7d4369e8c21dd7
Mohammadreza <m.r.alirad75@gmail.com>
2025-08-24 16:22:46 +0330

    Log Trigger Automation

---
+++ contracts/automation/log-trigger/EventEmitter.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: MIT  
+pragma solidity ^0.8.26;
+
+contract EventEmitter {
+    event WantsToCount(address indexed msgSender);
+
+    constructor() {}
+
+    function emitCountLog() public {
+        emit WantsToCount(msg.sender);
+    }
+}
